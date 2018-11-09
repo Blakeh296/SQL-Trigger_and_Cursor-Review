ad5a9aff79f5b937d11f8a8c75605257f9b78406
Blake Hoelle <bhoelle216@gmail.com>
2018-11-08 20:23:49 -0500

    commit

--- AdvWorks2012_sp_SalesByTerritoryWITHOUTCURSOR.sql
+++ AdvWorks2012_sp_SalesByTerritoryWITHOUTCURSOR.sql
@@ -7,17 +7,17 @@ GO
 CREATE PROC sp_SalesByTerritoryWITHOUTCURSOR
 	@year as date
 AS
-BEGIN
-	SELECT   MAX(st.TerritoryID) as Territory_ID, st.Name as Territory_Name
-	,ROUND(SUM(soh.TotalDue), 2) as Total_Sales, LEFT(@year, 4) as Fiscal_Year
+	BEGIN
+			SELECT   MAX(st.TerritoryID) as Territory_ID, st.Name as Territory_Name
+			,ROUND(SUM(soh.TotalDue), 2) as Total_Sales, LEFT(@year, 4) as Fiscal_Year
 
-    FROM  [Sales].[SalesOrderHeader] soh
-	INNER JOIN [Sales].[SalesTerritory] st
-	ON soh.TerritoryID = st.TerritoryID
+			FROM  [Sales].[SalesOrderHeader] soh
+			INNER JOIN [Sales].[SalesTerritory] st
+			ON soh.TerritoryID = st.TerritoryID
 
-    WHERE soh.OrderDate = @year AND soh.TerritoryID = st.TerritoryID  
-	GROUP BY st.Name
-	ORDER BY st.Name desc;
-END
+			WHERE soh.OrderDate = @year AND soh.TerritoryID = st.TerritoryID  
+			GROUP BY st.Name
+			ORDER BY st.Name desc;
+	END
 
 --EXEC sp_SalesByTerritoryWITHOUTCURSOR @year = '2006' 
